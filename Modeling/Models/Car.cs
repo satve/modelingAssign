6286fa919d5f6e530a6b0b4daccb07afaf285b85
Satveer Dhillon <Satveerdhillon01@gmail.com>
2018-09-21 09:45:29 -0500

    Initial commit

---
+++ Modeling/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Modeling.Models
+{
+    public class Car
+    {
+        public int Cost { get; set; }
+        public int Year { get; set; }
+        public String Make { get; set; }
+        public String Model { get; set; }
+        public string Color { get; set; }
+       
+    }
+}
---
+++ Modeling/Models/House.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Modeling.Models
+{
+    public class House
+    {
+        public int Value { get; set; }
+        public string LotSize { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int PostalCode { get; set; }
+
+    }
+}
